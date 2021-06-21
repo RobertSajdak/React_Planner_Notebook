443fd6c5a81c20673f3fc23bc42b159934ec2f41
RobertSajdak <robert.sajdak@onet.eu>
2021-06-21 23:21:01 +0200

    Komponent App ze statycznymi div'ami.

---
+++ react-planner/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <div className="countdown">
+            <strong>pobudka</strong> - 06:00
+        </div>
+        <div className="countdown">
+            <strong>śniadanie</strong> - 06:30
+        </div>
+    </div>
+);
+
+export default App;
