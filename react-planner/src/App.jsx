288479b5b3fc1162b8fbeda93e229cbff43ab653
RobertSajdak <robert.sajdak@onet.eu>
2021-06-26 16:03:26 +0200

    #6 Dodano const events z f-cją map().

--- react-planner/src/App.jsx
+++ react-planner/src/App.jsx
@@ -8,17 +8,16 @@ class App extends Component { // Wywołanie komponentu stanu.
         this.state = {
             events: [
                 {id: 0, name: "pobudka", time: "06:30"},
-                {id: 1, name: "śniadanie", time: "07:00"}
+                {id: 1, name: "trening", time: "06:45"},
+                {id: 2, name: "śniadanie", time: "07:30"}
             ]
-        }
+        };
     }
-    render() {
-        return (
-            <div>
-                <Countdown name="pobudka" time="06:30"/>
-                <Countdown name="śniadanie" time="07:00"/>
-            </div>
-        )
+    render() { // Generowanie komponentu na podstawie stanu.
+        const events = this.state.events.map(el => {
+            return <Countdown name={el.name} time={el.time}/>
+        })
+        return <div>{events}</div>
     }
 }
 
