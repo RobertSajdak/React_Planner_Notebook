63c83bd4377a74f091d460075eac49c8178b1fd6
RobertSajdak <robert.sajdak@onet.eu>
2021-06-26 15:42:17 +0200

    #5 Stworzono komponent stanu.

--- react-planner/src/App.jsx
+++ react-planner/src/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Countdown from "./Countdown.jsx";
 
-const App = () => (
-    <div>
-        <Countdown name="pobudka" time="06:30"/>
-        <Countdown name="śniadanie" time="07:00"/>
-    </div>
-);
+class App extends Component { // Wywołanie komponentu stanu.
+    constructor() {
+        super();
+        this.state = {
+            events: [
+                {id: 0, name: "pobudka", time: "06:30"},
+                {id: 1, name: "śniadanie", time: "07:00"}
+            ]
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Countdown name="pobudka" time="06:30"/>
+                <Countdown name="śniadanie" time="07:00"/>
+            </div>
+        )
+    }
+}
 
 export default App;
