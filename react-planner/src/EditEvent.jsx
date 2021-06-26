49b942d1d836987b9eb06c4532397bbab3bb3489
RobertSajdak <robert.sajdak@onet.eu>
2021-06-26 21:40:16 +0200

    #11 Odczytywanie wartości input.

--- react-planner/src/EditEvent.jsx
+++ react-planner/src/EditEvent.jsx
@@ -5,16 +5,31 @@ const EditEvent = props => {
     return (
         <div className="edit-event">
             <div className="edit-event__input-group">
-                <label htmlFor="name">name: </label>
-                <input type="text" id="name" name="name"/>
+                <label htmlFor="name">name : </label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={(e) => console.log(e.target.name, e.target.value)}
+                />
             </div>
             <div className="edit-event__input-group">
-                <label htmlFor="hour">hour: </label>
-                <input type="tel" id="hour" name="hour"/>
+                <label htmlFor="hour">hour : </label>
+                <input
+                    type="tel"
+                    id="hour"
+                    name="hour"
+                    onChange={(e) => console.log(e.target.name, e.target.value)}
+                />
             </div>
             <div className="edit-event__input-group">
-                <label htmlFor="minute">minute: </label>
-                <input type="tel" id="minute" name="minute"/>
+                <label htmlFor="minute">minute : </label>
+                <input
+                    type="tel"
+                    id="minute"
+                    name="minute"
+                    onChange={(e) => console.log(e.target.name, e.target.value)}
+                />
             </div>
             <button onClick={() => props.onSave()}>OK</button>
             <button>Cancel</button>
